681611247336fd20bdadb2d9ac5671ba9a0dbade
zoryon <gioelespata@gmail.com>
2025-02-21 23:18:12 +0100

    modified policy access

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,7 @@ export async function middleware(req: NextRequest) {
 
     // Handle QR codes page access
     if (req.nextUrl.pathname.startsWith("/qrcodes/")) {
-        NextResponse.next();
+        return NextResponse.next();
     }
 
     // Protect all other routes
