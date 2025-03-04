f5aa6a25ce20e9147bdee6eaa8b67354b0a33e51
zoryon <gioelespata@gmail.com>
2025-03-04 19:40:07 +0100

    fixing particle field

--- src/components/LandingComp/ParticleField.tsx
+++ src/components/LandingComp/ParticleField.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const ParticleField = ({ count }: { count: number }) => {
     return (
         <div className="absolute inset-0 pointer-events-none">
-            {[...Array(25)].map((_, i) => (
+            {[...Array(count)].map((_, i) => (
                 <motion.div
                     key={i}
                     className="absolute w-2 h-2 bg-cyan-400 rounded-full"
